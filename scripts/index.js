dcf6fa675be0310e7c97ee14d14b9ffcadede787
gleonio <gleonio@gmail.com>
2024-04-18 22:43:30 -0700

    implemented opening and closing of modal box

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,18 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+// console.log(initialCards);
+
+// Elements
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const modalCloseButton = document.querySelector("#modal-close-button");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+
+modalCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
